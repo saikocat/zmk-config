8ba341437dff09585a73604b5cbea9c87a6f013a
Hoa <hoameomu@gmail.com>
2023-06-18 00:24:29 +0800

    [uspc] add nice view spi macro for non tx pin

---
+++ config/userspace/nice_view_spi.h
@@ -0,0 +1,11 @@
+// Copyright 2023 saikocat (@saikocat)
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+// clang-format off
+#define NICEVIEW_SPI(board, cs_pin)                 \
+    &nice_view_spi {                                \
+        cs-gpios = <board cs_pin GPIO_ACTIVE_HIGH>; \
+    };
+// clang-format on
